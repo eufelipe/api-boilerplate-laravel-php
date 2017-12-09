9a27b9ba9f5926a42eef65d39a985cfa12d42f3a
Felipe Rosas <contato@eufelipe.com>
2017-12-09 11:48:03 -0200

    Implementação de Cache em TasksController

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,24 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use App\Http\Requests\TasksRequest as Request;
+use Carbon\Carbon;
 
 class TasksController extends Controller
 {
 
+    const TASK_TIME_CACHE_IN_MINUTES = 10;
+    const TASK_CACHE_KEY = 'api::tasks';
+
+
     public function index()
     {
-        return Task::all();
+        $timeCacheInMinutes = Carbon::now()->addMinutes(self::TASK_TIME_CACHE_IN_MINUTES);
+        $tasks = \Cache::remember(self::TASK_CACHE_KEY, $timeCacheInMinutes, function () {
+            return Task::all();
+        });
+        return $tasks;
+
     }
 
     public function store(Request $request)
     {
+        $this->cleanCache();
         $data = $request->all();
         $data['user_id'] = \Auth::user()->id;
         return Task::create($data);
     }
 
+
     public function update(Request $request, Task $task)
     {
+        $this->cleanCache();
         $this->authorize('update', $task);
         $task->update($request->all());
         return $task;
@@ -35,10 +48,15 @@ class TasksController extends Controller
 
     public function destroy(Task $task)
     {
+        $this->cleanCache();
         $this->authorize('delete', $task);
         $task->delete();
         return $task;
     }
 
+    private function cleanCache()
+    {
+        \Cache::forget(self::TASK_CACHE_KEY);
+    }
 
 }
