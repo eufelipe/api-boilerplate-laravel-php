8e42788384092b06767d9db8f8358e81ef4c5beb
Felipe Rosas <contato@eufelipe.com>
2017-12-09 11:02:53 -0200

    Configuracao do Passport para autenticação via tokens

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,24 +8,33 @@ use App\Http\Requests\UsersRequest as Request;
 class UsersController extends Controller
 {
 
-    public function index() {
+    public function index()
+    {
         return User::all();
     }
 
-    public function store(Request $request) {
-        return User::create($request->all());
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['password'] = bcrypt($request->get('password'));
+        return User::create($data);
     }
 
-    public function update(Request $request, User $user) {
-        $user->update($request->all());
+    public function update(Request $request, User $user)
+    {
+        $data = $request->all();
+        $data['password'] = bcrypt($request->get('password'));
+        $user->update($data);
         return $user;
     }
 
-    public function show(User $user) {
+    public function show(User $user)
+    {
         return $user;
     }
 
-    public function destroy(User $user) {
+    public function destroy(User $user)
+    {
         $user->delete();
         return $user;
     }
